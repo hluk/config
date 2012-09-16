c70c6414e94a1bec46789c8fd792c2ca797fdcea
Lukas Holecek <hluk@email.cz>
2012-09-16 08:43:22 +0200

    updated

--- bin/qtcreator.sh
+++ bin/qtcreator.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #bin=qtcreator
-bin=~/apps/qtcreator-build-release/bin/qtcreator
+bin=~/apps/qtcreator-build-release/bin/qtcreator.sh
+#bin=~/apps/qtcreator-build-test/bin/qtcreator.sh
 
 PATH=/usr/lib/ccache/bin:$PATH
 ccache --max-size=8G
 
-exec $bin -lastsession
+ulimit -c unlimited
+[ "`ulimit -c`" = "unlimited" ] || { echo "Core files cannot be generated!"; exit 1; }
+
+exec "$bin" -lastsession
 
