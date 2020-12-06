f1b260cd26aaf9193549df03e17b938cb5dc292e
Lukas Holecek <hluk@email.cz>
2020-12-06 18:27:39 +0100

    Update

--- bin/pomodoro.py
+++ bin/pomodoro.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+Simple pomodoro timer to show status in waybar (panel for sway window manager).
+
+Example configuration in "~/.config/waybar/config":
+
+    "custom/pomodoro": {
+        "interval": 60,
+        "on-click": "~/dev/bin/pomodoro.py next",
+        "on-click-right": "~/dev/bin/pomodoro.py prev",
+        "on-scroll-up": "~/dev/bin/pomodoro.py up",
+        "on-scroll-down": "~/dev/bin/pomodoro.py down",
+        "exec": "sh -c ~/dev/bin/pomodoro.py",
+        "return-type": "json"
+    },
+
+Using "sh -c" in the above fixes issues with updating the status.
+"""
+import json
 import getpass
 import math
 import sys
@@ -15,7 +33,7 @@ class Work(Interval):
 
 
 class Break(Interval):
-    name = "break"
+    name = "🍅 break"
 
 
 def load_state(state_file):
@@ -36,6 +54,7 @@ WORK = Work(minutes=25)
 SHORT = Break(minutes=5)
 LONG = Break(minutes=30)
 SHORT_COUNT = 4
+SCROLL_MINUTES = 2
 STATE_FILE = f"/tmp/pomodoro-{getpass.getuser()}"
 
 
@@ -48,15 +67,14 @@ def main():
 
     for cmd in sys.argv[1:]:
         if cmd == "next" or cmd == "prev":
-            if start == 0:
-                start = int(time())
-            else:
+            if start > 0:
                 d = 1 if cmd == "next" else -1
                 index = (index + d) % len(intervals)
-                start = 0
+            start = int(time())
         elif cmd == "up" or cmd == "down":
             if start > 0:
-                start += 60 if cmd == "up" else -60
+                d = 1 if cmd == "up" else -1
+                start += d * 60 * SCROLL_MINUTES
 
     interval = intervals[index]
     remaining_seconds = interval.minutes * 60
@@ -70,17 +88,17 @@ def main():
             interval = intervals[index]
             remaining_seconds = interval.minutes * 60
 
-    def info():
-        return (
-            f"{interval.name}"
-            f" {math.ceil(remaining_seconds / 60)}min"
-            f" ({index + 1}/{len(intervals)})"
-        )
+    remaining_minutes = math.ceil(remaining_seconds / 60)
+    paused = start == 0
+    symbol = "⏸️ " if paused else ""
+    status = "paused" if paused else "running"
+    out = {
+        "text": f"{symbol}{interval.name} <b>{remaining_minutes}</b>′",
+        "tooltip": f"pomodoro {status} {int(index/2) + 1}/{int(len(intervals)/2)}",
+        "class": status,
+    }
 
-    if start == 0:
-        print(f"[START?] {info()}")
-    else:
-        print(info())
+    print(json.dumps(out))
 
     if index0 != index or start0 != start:
         save_state(STATE_FILE, index, start)
