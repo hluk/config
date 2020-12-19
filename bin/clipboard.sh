a602cd558bc1b2bfce111e9d7a452c360b492404
Lukas Holecek <hluk@email.cz>
2020-12-19 09:10:07 +0100

    Update

--- bin/clipboard.sh
+++ bin/clipboard.sh
@@ -7,18 +7,18 @@ cmd=$rundir/$version/copyq
 
 # Workaround for bug in Qt 5.14.2.
 # https://bugreports.qt.io/browse/QTBUG-84363
-export QV4_FORCE_INTERPRETER=1
+#export QV4_FORCE_INTERPRETER=1
 
 export COPYQ_LOG_LEVEL=debug
 export COPYQ_DEFAULT_ICON=1
 
-# There are some issues accessing clipboard and notifications being focused.
-# See: https://github.com/hluk/CopyQ/issues/1243#issuecomment-549040797
-#export QT_QPA_PLATFORM=xcb
+if [[ $XDG_SESSION_TYPE == wayland ]]; then
+    #export QT_QPA_PLATFORM=xcb
+    export QT_QPA_PLATFORM=wayland
+fi
 
-export QT_QPA_PLATFORM=wayland
-
-export QT_QPA_PLATFORMTHEME=qt5ct
+#export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_STYLE_OVERRIDE=kvantum
 
 [ "$version" = "debug" ] && ulimit -c unlimited
 
--- bin/monitors.sh
+++ bin/monitors.sh
@@ -3,12 +3,19 @@ set -xeu
 
 out1="Lenovo Group Limited 0x40BA 0x00000000"
 out2="Goldstar Company Ltd LG Ultra HD 0x00003617"
+out1_enabled=${1:-enable}
+scale=${2:-2}
 
-s1=2
+s1=$scale
 w1=1920
 h1=1080
 
-s2=2
+if [[ $out1_enabled == "disable" ]]; then
+    echo "Disabling $out1"
+    w1=0
+fi
+
+s2=$scale
 w2=3840
 h2=2160
 
@@ -18,8 +25,11 @@ x2=$((w1/s1))
 y2=$((0))
 
 msg=$(cat <<EOD
+output "*" scale 1
+
 output "$out1" scale $s1
 output "$out1" pos $x1 $y1 res ${w1}x$h1
+output "$out1" $out1_enabled
 
 output "$out2" scale $s2
 output "$out2" pos $x2 $y2 res ${w2}x$h2
