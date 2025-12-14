1e8c0470653f199af9d0cc72e14dafced45fbf94
Lukas Holecek <hluk@email.cz>
2025-12-14 13:06:16 +0100

    Update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,8 +1,9 @@
 return {
-  -- enabled = false,
+  enabled = false,
   "CopilotC-Nvim/CopilotChat.nvim",
   dependencies = {
     { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+    -- { "zbirenbaum/copilot.lua" },
     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
   },
   build = "make tiktoken", -- Only on MacOS or Linux
